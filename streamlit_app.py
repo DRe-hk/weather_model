7af3ebdd042edc25ac418adb89e801c2ecb7fef7
DRe-hk <edersanca12@gmail.com>
2024-10-10 00:58:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,66 +1,26 @@
-import altair as alt
-import pandas as pd
 import streamlit as st
+import numpy as np
+import pickle
+# Título de la aplicación
+st.title('Predicción con K-Nearest Neighbors')
 
-# Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
-st.write(
-    """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
-    """
-)
+# Descripción de la aplicación
+st.write('Ajusta los sliders para introducir las características.')
 
+# Crear sliders para recibir valores de entrada
+feature1 = st.slider('Feature 1', 0.0, 10.0, 5.0)
+feature2 = st.slider('Feature 2', 0.0, 10.0, 5.0)
+feature3 = st.slider('Feature 3', 0.0, 10.0, 5.0)
+feature4 = st.slider('Feature 4', 0.0, 10.0, 5.0)
 
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
-def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
-    return df
+# Valores de entrada en un array
+input_data = np.array([[feature1, feature2, feature3, feature4]])
 
-
-df = load_data()
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
-
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
+knn = pickle.load(open('model.pkl', 'rb'))
 
 
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
+# Hacer la predicción
+prediction = knn.predict(input_data)
 
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
+# Mostrar la predicción
+st.write('Predicción:', [prediction][0])
